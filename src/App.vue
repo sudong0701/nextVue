2827009081ed315d90ff01aa86495ec5fdd106fa
sudong.duan <1006364428@qq.com>
2021-06-28 17:18:02 +0800

    <feat>[src/App.vue]:新增主题色配置、新增路由拦截器

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,58 @@
 <template>
     <div class="app">
         <n-message-provider>
-            <router-view>
-
-            </router-view>
+            <router-view></router-view>
         </n-message-provider>
     </div>
 
 </template>
 
 <script lang="ts">
-import { NMessageProvider } from "naive-ui";
-import { defineComponent } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { NMessageProvider, NConfigProvider, darkTheme } from "naive-ui";
+import { defineComponent, onMounted } from "vue";
+import { menuOptions } from "./utils/config";
+import { isEmpty } from "./utils/common";
+import { menuItemConfig } from "./utils/config";
 
 export default defineComponent({
     name: "App",
     components: {
         NMessageProvider,
+        NConfigProvider,
     },
     setup() {
+        const router = useRouter();
 
+        router.beforeEach((to, from, next) => {
+            if (isEmpty(to.name)) {
+                next({ name: "404" });
+            } else {
+                let isMatch: boolean = false;
+                for (let i = 0; i < menuOptions.length; i++) {
+                    if (isEmpty(menuOptions[i].children)) {
+                        if (menuOptions[i].key === to.name) {
+                            isMatch = true;
+                        }
+                    } else {
+                        for ( let j = 0; j < (menuOptions[i].children as Array<menuItemConfig>).length; j++ ) {
+                            if ((menuOptions[i].children as Array<menuItemConfig>)[j].key === to.name) {
+                                isMatch = true;
+                            }
+                        }
+                    }
+                }
+                if (isMatch) {
+                    next();
+                } else {
+                    next({ name: "404" });
+                }
+            }
+        });
+
+        return {
+            darkTheme,
+        };
     },
 });
 </script>
