a94bb60b46b0a6367c7ee99744b0ef3265a50cbc
sudong.duan <1006364428@qq.com>
2021-06-29 17:10:16 +0800

    <feat>[src/page/main/list/userList/index.vue]:增加vuex存值的示例

--- src/page/main/list/userList/index.vue
+++ src/page/main/list/userList/index.vue
@@ -7,23 +7,32 @@
 <script lang="ts">
 import { h, defineComponent } from "vue";
 import { NDataTable, NTag, NButton } from "naive-ui";
+import { useStore } from 'vuex'
 
-const createColumns = () => {
+interface createDataConfig {
+    key: number
+    name: string
+    age: number
+    address: string
+    tags: Array<string>
+}
+
+const createColumns = ( changeUsername: Function ) => {
     return [
         {
-            title: "Name",
+            title: "姓名",
             key: "name",
         },
         {
-            title: "Age",
+            title: "年龄",
             key: "age",
         },
         {
-            title: "Address",
+            title: "地址",
             key: "address",
         },
         {
-            title: "Tags",
+            title: "标签",
             key: "tags",
             render(row: any) {
                 const tags = row.tags.map((tagKey: any) => {
@@ -44,42 +53,50 @@ const createColumns = () => {
             },
         },
         {
-            title: "Action",
+            title: "操作",
             key: "actions",
-            render(row: any) {
+            render(row: createDataConfig) {
                 return h(
                     NButton,
                     {
                         size: "small",
+                        onClick: () => changeUsername(row),
                     },
-                    { default: () => "Send Email" }
+                    { default: () => "改变用户" }
                 );
             },
         },
     ];
 };
 
-const createData = () => [
+const createData= ():Array<createDataConfig> => [
     {
         key: 0,
-        name: "John Brown",
-        age: 32,
-        address: "New York No. 1 Lake Park",
-        tags: ["nice", "developer"],
+        name: "树洞丶",
+        age: 27,
+        address: "江苏省南京市雨花台区",
+        tags: ["游戏人生", "程序员"],
     },
     {
         key: 1,
-        name: "Jim Green",
-        age: 42,
-        address: "London No. 1 Lake Park",
-        tags: ["wow"],
+        name: "龙少丶",
+        age: 27,
+        address: "山东省青岛市即墨区",
+        tags: ["酒桶", "程序员"],
     },
     {
         key: 2,
-        name: "Joe Black",
-        age: 32,
-        address: "Sidney No. 1 Lake Park",
-        tags: ["cool", "teacher"],
+        name: "林少丶",
+        age: 23,
+        address: "内蒙古自治区鄂尔多斯市",
+        tags: ["搞笑担当"],
+    },
+    {
+        key: 3,
+        name: "举少丶",
+        age: 24,
+        address: "广东省广州市",
+        tags: ["运动大人", "程序员"],
     },
 ];
 
@@ -90,9 +107,20 @@ export default defineComponent({
         NButton,
     },
     setup() {
+        const store = useStore()
+
+        /**
+         * 改变vuex里的username
+         * @param
+         * @return
+         */
+        const changeUsername = (rowData:createDataConfig) => {
+            store.commit('SET_USERNAME', rowData.name )
+        };
+
         return {
             data: createData(),
-            columns: createColumns(),
+            columns: createColumns(changeUsername),
             pagination: {
                 pageSize: 10,
             },
