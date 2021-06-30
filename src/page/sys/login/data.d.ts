1cf6458bca9cb41901c84d35f7820e2cffdfce73
sudong.duan <1006364428@qq.com>
2021-06-30 14:59:23 +0800

    <feat>[src/page/sys/login/data.d.ts]:新增数据格式接口文件

---
+++ src/page/sys/login/data.d.ts
@@ -0,0 +1,19 @@
+declare interface userInfoConfig {
+	address: string,
+	age: number,
+	birthday: string,
+	createTime: string,
+	hobby: string,
+	id: string,
+	introduction: string,
+	job: string,
+	name: string,
+	nickname: string | null,
+	password: string,
+	phone: number,
+	region: string,
+	regionName: string,
+	sex: string,
+	updateTime: string,
+	username: number
+}
